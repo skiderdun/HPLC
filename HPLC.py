8a64e964e15b7223905e57ce8a3664a1561c141a
skiderdun <skiderdun@gmail.com>
2022-09-25 10:50:31 -0500

    HPLC.py

---
+++ HPLC.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+import os
+
+def create(path):
+    Data = pd.DataFrame()
+    dirs = os.listdir(path)
+    for file in dirs:
+            Data = pd.concat([Data, pd.read_excel(f"{path}/{file}")])
+    Data.index = Data['Unnamed: 0']
+    Data.index.name = 'Compound'
+    Data.dropna(how = 'all', inplace =True)
+    Data.drop(columns = "Unnamed: 0", inplace = True)
+    Data.drop(index = 'Name', inplace = True)
+    Data.reset_index(inplace = True)
+    Data["Unnamed: 1"].fillna(method = 'ffill', inplace = True)
+    Data.sort_index(inplace = True)
+    Data.index = Data.Compound
+    Data.drop("Sample_Name" , inplace = True)
+    Data.drop("Compound" , axis = 1, inplace = True)
+    Data.index = pd.MultiIndex.from_tuples(list(zip(Data.index.to_list(), Data["Unnamed: 1"].to_list())))
+    Data.drop("Unnamed: 1", axis = 1, inplace = True)
+    Data.sort_index(inplace = True)
+    
+    return Data
+
+def ind_col(frame):
+    new_ind = pd.MultiIndex.from_tuples([tuple(x.split()) for x in list(set([x[1] for x in df.index.to_list()]))])
+    new_cols = list(set([x[0] for x in frame.index.to_list()]))
+    tempframe = pd.DataFrame(columns = new_cols,index = new_ind)
+    for i in frame.iterrows():
+        val = i[1].values.astype(float)[0]
+        tempframe.at[tuple(i[0][1].split())] = val
+        print(val)
+        print(i)
+    return tempframe
+
+def writeOut(name, df):
+    with pd.ExcelWriter(name) as writer:
+        df.to_excel(writer, sheet_name = "Data") 
+    return 'Good to go!'
+
+def repl(df):
+    df = df 
+    while True :
+        inp = input("REPL >> ")
+        print(eval(inp))
+        if inp == quit:
+            break
+
+def SwitchKey(inp, df):
+    df = df
+    switch={
+            'new' : lambda x : create(x),
+            'format' : lambda x : ind_col(x),
+            'write' : lambda x : writeOut(x, df),
+            'test' : lambda x : x,
+            'REPL' : lambda x : repl(x),
+            'quit' : lambda x : quit()
+            }
+    return switch.get(inp)
+
+def main():
+    df = pd.DataFrame()
+    print(f'new : path to hplc files -> raw frame\nformat : raw frame -> frame with formated index and column\nwrite : file name -> excel file')
+    while True:
+        inp = input('\n--> ').split()
+        try: 
+            callable(SwitchKey(inp[0], df))
+        except Exception:
+            print("WRONG\ntry again dumbass")
+            pass
+        finally:
+
+            if len(inp) == 2:
+                df = SwitchKey(inp[0], df)(inp[1])
+                print(df)
+            else:
+                df = SwitchKey(inp[0])(df)
+                print(df)
+
+if __name__ == "__main__":
+    main()
