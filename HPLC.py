88a9a198baddc0341a8db13813d3517b7684776e
skiderdun <skiderdun@gmail.com>
2022-09-26 15:19:37 -0500

    Newest version

--- HPLC.py
+++ HPLC.py
@@ -4,38 +4,54 @@ import os
 
 def create(path):
     Data = pd.DataFrame()
-    dirs = os.listdir(path)
-    for file in dirs:
+    try: 
+        dirs = os.listdir(path)
+    except Exception:
+        print("That path isn't real\nfrigin noob\n")
+        continue
+    else:
+        for file in dirs:
             Data = pd.concat([Data, pd.read_excel(f"{path}/{file}")])
-    Data.index = Data['Unnamed: 0']
-    Data.index.name = 'Compound'
-    Data.dropna(how = 'all', inplace =True)
-    Data.drop(columns = "Unnamed: 0", inplace = True)
-    Data.drop(index = 'Name', inplace = True)
-    Data.reset_index(inplace = True)
-    Data["Unnamed: 1"].fillna(method = 'ffill', inplace = True)
-    Data.sort_index(inplace = True)
-    Data.index = Data.Compound
-    Data.drop("Sample_Name" , inplace = True)
-    Data.drop("Compound" , axis = 1, inplace = True)
-    Data.index = pd.MultiIndex.from_tuples(list(zip(Data.index.to_list(), Data["Unnamed: 1"].to_list())))
-    Data.drop("Unnamed: 1", axis = 1, inplace = True)
-    Data.sort_index(inplace = True)
+        Data.index = Data['Unnamed: 0']
+        Data.index.name = 'Compound'
+        Data.dropna(how = 'all', inplace =True)
+        Data.drop(columns = "Unnamed: 0", inplace = True)
+        Data.drop(index = 'Name', inplace = True)
+        Data.reset_index(inplace = True)
+        Data["Unnamed: 1"].fillna(method = 'ffill', inplace = True)
+        Data.sort_index(inplace = True)
+        Data.index = Data.Compound
+        Data.drop("Sample_Name" , inplace = True)
+        Data.drop("Compound" , axis = 1, inplace = True)
+        Data.index = pd.MultiIndex.from_tuples(list(zip(Data.index.to_list(), Data["Unnamed: 1"].to_list())))
+        Data.drop("Unnamed: 1", axis = 1, inplace = True)
+        Data.sort_index(inplace = True)
     
-    return Data
+        return Data
 
 def ind_col(frame):
-    new_ind = pd.MultiIndex.from_tuples([tuple(x.split()) for x in list(set([x[1] for x in df.index.to_list()]))])
     new_cols = list(set([x[0] for x in frame.index.to_list()]))
+
+    ind_len = int(0)
+    for col in new_cols:
+        length = frame.loc[col].size
+        if length > ind_len:
+            ind_len = int(length) 
+
+    new_ind = pd.MultiIndex.from_tuples([tuple(y.split()) for y in list([x[1] for x in frame.index.to_list()])[0:ind_len]])
+
     tempframe = pd.DataFrame(columns = new_cols,index = new_ind)
-    for i in frame.iterrows():
-        val = i[1].values.astype(float)[0]
-        tempframe.at[tuple(i[0][1].split())] = val
-        print(val)
-        print(i)
+
+
+    for col in new_cols:
+        for i in enumerate(frame.loc[col].values.astype(float).tolist()):
+            tempframe[col].iloc[i[0]] = i[1][0]
+
     return tempframe
 
 def writeOut(name, df):
+    if name[-5:] != ".xlsx":
+        name = name + '.xlsx'
     with pd.ExcelWriter(name) as writer:
         df.to_excel(writer, sheet_name = "Data") 
     return 'Good to go!'
@@ -52,11 +68,11 @@ def SwitchKey(inp, df):
     df = df
     switch={
             'new' : lambda x : create(x),
-            'format' : lambda x : ind_col(x),
+            'format' : lambda : ind_col(df),
             'write' : lambda x : writeOut(x, df),
             'test' : lambda x : x,
             'REPL' : lambda x : repl(x),
-            'quit' : lambda x : quit()
+            'quit' : lambda : quit()
             }
     return switch.get(inp)
 
@@ -66,17 +82,20 @@ def main():
     while True:
         inp = input('\n--> ').split()
         try: 
-            callable(SwitchKey(inp[0], df))
-        except Exception:
-            print("WRONG\ntry again dumbass")
-            pass
-        finally:
+            if len(inp) == 2:
+                SwitchKey(inp[0], df)(inp[1])
+            else:
+                SwitchKey(inp[0], df)()
+
+        except TypeError:
+            continue
+        else:
 
             if len(inp) == 2:
                 df = SwitchKey(inp[0], df)(inp[1])
                 print(df)
             else:
-                df = SwitchKey(inp[0])(df)
+                df = SwitchKey(inp[0], df)()
                 print(df)
 
 if __name__ == "__main__":
